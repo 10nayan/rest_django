01b0ddd01cecced3175da20c229b136b52de4f9c
10nayan <nayanjyotipatowary@gmail.com>
2020-10-15 11:27:51 +0530

    second commit

--- pilot/views.py
+++ pilot/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Movie
 from rest_framework import routers, serializers, viewsets
-from .serializer import UserSerializer
+from .serializers import UserSerializer,MovieSerializer
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset=Movie.objects.all()
+    serializer_class=MovieSerializer
